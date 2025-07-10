3e21bed6d639af48d7cb91eac03db31bdaeef8a2
luantyMG <angelot_123@hotmail.com>
2025-07-09 21:33:01 -0600

    news

--- app/(tabs)/news.tsx
+++ app/(tabs)/news.tsx
@@ -5,6 +5,7 @@ import NewsCard from '../../components/NewsCard/NewsCard';
 import newsData from '../../src/data/newsData';
 import { useTheme } from '../../src/context/ThemeContext/ThemeContext';
 import { StatusBar } from 'expo-status-bar';
+import TabsLayoutWrapper from '@/components/TabsLayoutWrapper/TabsLayoutWrapper';
 
 const PAGE_SIZE = 5;
 
@@ -34,32 +35,38 @@ export default function NewsScreen() {
   }, []);
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
-       <StatusBar style={colors.background === '#121212' ? 'light' : 'dark'} />
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <NewsCard
-            avatarUrl={item.avatarUrl}
-            title={item.title}
-            date={item.date}
-            description={item.description}
-            imageUrl={item.imageUrl}
-          />
-        )}
-        contentContainerStyle={{ padding: 10 }}
-        onEndReached={loadMore}
-        onEndReachedThreshold={0.5}
-        refreshing={refreshing}
-        onRefresh={onRefresh}
-        ListFooterComponent={() =>
-          data.length < newsData.length ? (
-            <ActivityIndicator size="large" color={colors.primary} />
-          ) : null
-        }
-      />
-    </SafeAreaView>
+    <TabsLayoutWrapper>
+      <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
+        <StatusBar style={colors.background === '#121212' ? 'light' : 'dark'} />
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <NewsCard
+              avatarUrl={item.avatarUrl}
+              title={item.title}
+              date={item.date}
+              description={item.description}
+              imageUrl={item.imageUrl}
+            />
+          )}
+          contentContainerStyle={{
+            paddingTop: 10,
+            paddingBottom: 20,
+          }}
+          onEndReached={loadMore}
+          onEndReachedThreshold={0.5}
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          ListFooterComponent={() =>
+            data.length < newsData.length ? (
+              <ActivityIndicator size="large" color={colors.primary} />
+            ) : null
+          }
+        />
+      </SafeAreaView>
+    </TabsLayoutWrapper>
+
   );
 }
 
