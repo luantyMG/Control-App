aaa04343d8b806c4042bc77b43874060ceae426a
luantyMG <angelot_123@hotmail.com>
2025-07-30 00:11:53 -0600

    Reorganizacion

--- src/components/NewsCard/types.ts
+++
@@ -1,7 +0,0 @@
-export interface NewsCardProps {
-    avatarUrl: string;
-    title: string;
-    date: string;
-    description: string;
-    imageUrl: string;
-}
--- src/components/ProfileCard/types.ts
+++
@@ -1,6 +0,0 @@
-export interface ProfileCardProps {
-    nombre : string;
-    edad: number;
-    onPress: () => void;
-    sexo: string;
-}
